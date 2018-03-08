9fc2675a269c4d3b5d8a8d1315e853a265f6dbf8
Anicet Serge <tghil7@aol.com>
2018-03-07 20:30:42 -0600

    Modified locations list view to use knockout observables

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Neighborhood-map
+This application displays a list of must-visit places in downtown Kansas City.
+- Open the file index.html in a text editor such as notepad++.
+- In notepad++, click the "Run" tab, and select Launch in ...(your favorite browser) 
+- The app will open, and you can click on the hamburger button on the left to expand the menu.
+- The menu gives you the option to filter those favorites places based on categories, such as restaurants, entertainment etc...
+
+
+References:
+http://api.jquery.com/jquery.ajax/#jqXHR
+https://developer.foursquare.com/docs/api/venues/explore
+https://blueprintinteractive.com/blog/html5-geolocation-venue-search-powered-foursquare-and-retina-maps
+Udacity forum https://discussions.udacity.com/t/need-help-with-locations-filter-functionality/228119
+http://knockoutjs.com/documentation/visible-binding.html
+http://knockoutjs.com/documentation/options-binding.html
--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var map;
-var markers= [];
+var markers = [];
 var infowindows = [];
 var geocoder;
 var visible;
@@ -8,186 +8,181 @@ var CLIENT_SECRET = "KCVBNGHC3P4RV401J0RJOVP4RNJ1GJL3PCUPVYAAIRM2ZFEE";
 
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 39.098005, lng: -94.581834},
-    zoom: 15
-    });
-	
-	geocoder = new google.maps.Geocoder();
-	ko.applyBindings(new ViewModel());
-	
+    try {
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                lat: 39.098005,
+                lng: -94.581834
+            },
+            zoom: 15
+        });
+    } catch (e) {
+        alert("Something went wrong. Unable to load the map.");
+    }
+
+    geocoder = new google.maps.Geocoder();
+    ko.applyBindings(new ViewModel());
+
 }
 
 
-
-
-
-
 //Creation of the array of locations in Kansas City
-var kcList = [
-		{
-			 name:'Up-Down KC',
-          address: '101 Southwest Blvd, Kansas City, MO 64108',
-		       id: '5531913c498ea6903c5eabc1',
-		 category:'Entertainment',
-		  visible: ko.observable(true)
-		
-		},
-		
-		{
-			 name: 'Jack Stack Barbecue',
-		  address: '101 W 22nd St #300, Kansas City, MO 64108',
-			   id: '4ad92984f964a520b31821e3',
-		 category:'Restaurants',
-		  visible: ko.observable(true)
-		},
-		{
-		   	 name: 'Union Station Kansas City',
-		  address: '30 W Pershing Rd, Kansas City, MO 64108',
-		       id: '4f885099e4b0cec3aa2c928a',
-		 category:'Museums and Monuments',
-		  visible: ko.observable(true)
-		},
-		
-		{
-			 name: 'LEGOLAND Discovery Center Kansas City',
-		  address: '2475 Grand Blvd, Kansas City, MO 64108',
-		       id:'4e83423c93ad9516fc9aa3a5',
-		 category:'Entertainment',
-		  visible: ko.observable(true)
-		},
-		
-		{
-			 name: 'National World War I Museum and Memorial',
-		  address: '2 Memorial Dr, Kansas City, MO 64108',
-			   id:'4f9c0ba5e4b04dd7353a754d',
-		 category:'Museums and Monuments',
-		  visible: ko.observable(true)
-		}
+var kcList = [{
+        name: 'Up-Down KC',
+        address: '101 Southwest Blvd, Kansas City, MO 64108',
+        id: '5531913c498ea6903c5eabc1',
+        category: 'Entertainment',
+        visible: ko.observable(true)
+
+    },
+
+    {
+        name: 'Jack Stack Barbecue',
+        address: '101 W 22nd St #300, Kansas City, MO 64108',
+        id: '4ad92984f964a520b31821e3',
+        category: 'Restaurant',
+        visible: ko.observable(true)
+    },
+    {
+        name: 'Union Station Kansas City',
+        address: '30 W Pershing Rd, Kansas City, MO 64108',
+        id: '4f885099e4b0cec3aa2c928a',
+        category: 'Museums and Monuments',
+        visible: ko.observable(true)
+    },
+
+    {
+        name: 'LEGOLAND Discovery Center Kansas City',
+        address: '2475 Grand Blvd, Kansas City, MO 64108',
+        id: '4e83423c93ad9516fc9aa3a5',
+        category: 'Entertainment',
+        visible: ko.observable(true)
+    },
+
+    {
+        name: 'National World War I Museum and Memorial',
+        address: '2 Memorial Dr, Kansas City, MO 64108',
+        id: '4f9c0ba5e4b04dd7353a754d',
+        category: 'Museums and Monuments',
+        visible: ko.observable(true)
+    }
 ];
 
 
-var ViewModel= function(){
-	var self= this;
-	var visible = ko.observable(true);
-	this.locations = ko.observableArray([]);
-	kcList.forEach(function(locationItem){
-	 geocodeAddress(geocoder, map, locationItem);
-	   self.locations.push(locationItem);		
-	});
-	
-	this.controlMarker = function (location){
-	//Show the element infowindow each time the list element is clicked
-	 markers.forEach(function(markerItem){
-			if (markerItem.name == location.name){
-			google.maps.event.trigger(markerItem.marker, 'click');
-			}
-			
-		});
-	
-	};
-	
-	this.filterLocation = function (){
-	 for (var i = 0; i < this.locations().length; i ++){
-	   //Get the selected category. If it matches the category of the current location in the array, show that location, or else hide it.
-		  if ($("#selectedCategory").val()==="All"){
-			  (this.locations()[i]).visible(true);
-		  }
-		  else if (this.locations()[i].category == ($("#selectedCategory").val())){
-			 (this.locations()[i]).visible(true);
-			}
-		 else if (this.locations()[i].category != ($("#selectedCategory").val())){ 
-				(this.locations()[i]).visible(false);
-			}
-			  
-		}
+var ViewModel = function() {
+    var self = this;
+    //var visible = ko.observable(true);
+    this.categories = ko.observableArray(["All", "Restaurant", "Museums and Monuments", "Entertainment"]);
+    this.selectedCategory = ko.observable("All");
+    this.locations = ko.observableArray([]);
+    kcList.forEach(function(locationItem) {
+        geocodeAddress(geocoder, map, locationItem);
+        self.locations.push(locationItem);
+    });
 
-//Show or hide the marker
-	markers.forEach(function(markerItem){
-	        if ($("#selectedCategory").val()==="All"){
-			    markerItem.marker.setVisible(true);
-				console.log("Nothing else to do here");
-		    } 
-			else if (markerItem.category == ($("#selectedCategory").val())){
-			 markerItem.marker.setVisible(true);
-			}
-			else if (markerItem.category != ($("#selectedCategory").val())){ 
-				markerItem.marker.setVisible(false);
-			}
-		//else{markerItem.marker.setVisible(true);}
-			
-		});	
-    };
-		
-	
+    this.controlMarker = function(location) {
+        //Show the element infowindow each time the list element is clicked
+        markers.forEach(function(markerItem) {
+            if (markerItem.name == location.name) {
+                google.maps.event.trigger(markerItem.marker, 'click');
+            }
 
-function geocodeAddress(geocoder, resultsMap, myLocation) {
-	  var address = myLocation.address;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
-            resultsMap.setCenter(results[0].geometry.location); 
-			 var infowindow = new google.maps.InfoWindow(); 
-			
-            //TODO: Insert code here to take the first result's formatted address, and LOCATION.
-            myLocation.marker = new google.maps.Marker({
-				map : resultsMap,
-				position: results[0].geometry.location,
-				animation: google.maps.Animation.DROP
-			});
-			
-			markers.push({name: myLocation.name,
-			            marker: myLocation.marker,
-						category:myLocation.category
-			});
-		
-//Ajax call to Foursquare API for information about my different locations. 
-			$.ajax({
-					type: "GET",
-					dataType: 'json',
-					cache: false,
-					async: true, 
-					url: "https://api.foursquare.com/v2/venues/"+ myLocation.id +"?client_id="+ CLIENT_ID +"&client_secret=" +CLIENT_SECRET+"&v=20180224"
-				})
-				.done(function( data ) { 
-					infowindow.setContent("<div> Name: "+ data.response.venue.name +  
-					"<br/>"+ " Contacts:" + data.response.venue.contact.formattedPhone + 
-					"<br/>"+ "Rating:" + data.response.venue.rating + "</div>");
-					
-	
-				})//Error message to post on the console.
-				.fail(function() {
-					console.log( "Unable to retrieve  foursquare data about this location ");
-				});
-			
-			myLocation.infowindow = infowindow;
-			infowindows.push(myLocation.infowindow);
-			//Create an onclick event to open an Infowindow at each marker
-			myLocation.marker.addListener('click', function(){
-			//Show the infoWindow
-			myLocation.infowindow.open(map, myLocation.marker);
-			myLocation.marker.setAnimation(google.maps.Animation.BOUNCE);
-			setTimeout(myLocation.marker.setAnimation(null), 15000);
-			
-			});
-		
-						
-			
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
         });
-		
-      }
-	  
+
+    };
+
+    this.filterLocation = function() {
+        for (var i = 0; i < this.locations().length; i++) {
+            //Get the selected category. If it matches the category of the current location in the array, show that location, or else hide it.
+            if (self.selectedCategory() === "All") {
+                (this.locations()[i]).visible(true);
+            } else if (this.locations()[i].category == self.selectedCategory()) {
+                (this.locations()[i]).visible(true);
+            } else if (this.locations()[i].category != self.selectedCategory()) {
+                (this.locations()[i]).visible(false);
+            }
+
+        }
+
+        //Show or hide the marker
+        markers.forEach(function(markerItem) {
+            if (self.selectedCategory() === "All") {
+                markerItem.marker.setVisible(true);
+                console.log("Nothing else to do here");
+            } else if (markerItem.category == self.selectedCategory()) {
+                markerItem.marker.setVisible(true);
+            } else if (markerItem.category != self.selectedCategory()) {
+                markerItem.marker.setVisible(false);
+            }
 
 
-		
-	
-		
+        });
+    };
+
+
+
+    function geocodeAddress(geocoder, resultsMap, myLocation) {
+        var address = myLocation.address;
+        geocoder.geocode({
+            'address': address
+        }, function(results, status) {
+            if (status === google.maps.GeocoderStatus.OK) {
+                resultsMap.setCenter(results[0].geometry.location);
+                var infowindow = new google.maps.InfoWindow();
+
+                //TODO: Insert code here to take the first result's formatted address, and LOCATION.
+                myLocation.marker = new google.maps.Marker({
+                    map: resultsMap,
+                    position: results[0].geometry.location,
+                    animation: google.maps.Animation.DROP
+                });
+
+                markers.push({
+                    name: myLocation.name,
+                    marker: myLocation.marker,
+                    category: myLocation.category
+                });
+
+                //Ajax call to Foursquare API for information about my different locations. 
+                $.ajax({
+                        type: "GET",
+                        dataType: 'json',
+                        cache: false,
+                        async: true,
+                        url: "https://api.foursquare.com/v2/venues/" + myLocation.id + "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&v=20180224"
+                    })
+                    .done(function(data) {
+                        infowindow.setContent("<div> Name: " + data.response.venue.name +
+                            "<br/>" + " Contacts:" + data.response.venue.contact.formattedPhone +
+                            "<br/>" + "Rating:" + data.response.venue.rating + "</div>");
+
+
+                    }) //Error message to post on the console.
+                    .fail(function() {
+                        alert("Unable to retrieve  foursquare data about this location ");
+                    });
+
+                myLocation.infowindow = infowindow;
+                infowindows.push(myLocation.infowindow);
+                //Create an onclick event to open an Infowindow at each marker
+                myLocation.marker.addListener('click', function() {
+                    //Show the infoWindow
+                    myLocation.infowindow.open(map, myLocation.marker);
+                    myLocation.marker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(myLocation.marker.setAnimation(null), 15000);
+
+                });
+
+
+
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
+
+    }
+
+
+
+
 };
-
-
-
-
-	
-
